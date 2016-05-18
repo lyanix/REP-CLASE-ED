35b5ff3b36e743550849497e53382f5d3538072f
Sara Fernández <sara.fdez@telefonica.net>
2016-05-18 19:45:18 +0200

    Combobox añadido

--- APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
+++ APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.cbPaises = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -52,11 +53,25 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // cbPaises
+            // 
+            this.cbPaises.FormattingEnabled = true;
+            this.cbPaises.Items.AddRange(new object[] {
+            "ESPAÑA",
+            "FRANCIA",
+            "ITALIA",
+            "ALEMANIA"});
+            this.cbPaises.Location = new System.Drawing.Point(139, 230);
+            this.cbPaises.Name = "cbPaises";
+            this.cbPaises.Size = new System.Drawing.Size(121, 21);
+            this.cbPaises.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(572, 391);
+            this.Controls.Add(this.cbPaises);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbPaises;
     }
 }
 
